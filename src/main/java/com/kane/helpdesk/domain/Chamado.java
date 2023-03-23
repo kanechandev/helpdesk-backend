721832f68662bc43195d997fde658527a9c551e5
Kane Chan <kanes.chan@gmail.com>
2023-03-23 16:27:28 -0300

    Anotações JPA para classes de domínio.

--- src/main/java/com/kane/helpdesk/domain/Chamado.java
+++ src/main/java/com/kane/helpdesk/domain/Chamado.java
@@ -1,22 +1,46 @@
 package com.kane.helpdesk.domain;
 
+import java.io.Serializable;
 import java.time.LocalDate;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.kane.helpdesk.domain.enums.Prioridade;
 import com.kane.helpdesk.domain.enums.Status;
 
-
-public class Chamado {
+@Entity
+public class Chamado implements Serializable{
     
+    private static final long serialVersionUID = 1L;
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
+
+    @JsonFormat(pattern = "dd/MM/yyyy")
     private LocalDate dataAbertura = LocalDate.now();
+
+    @JsonFormat(pattern = "dd/MM/yyyy")
     private LocalDate dataFechamento;
+
+
     private Prioridade prioridade;
     private Status status;
     private String titulo;
     private String observacoes;
 
+    @ManyToOne//Muitos chamados para um técnico
+    @JoinColumn(name = "tecnico_id")
     private Tecnico tecnico;
+
+    @ManyToOne//Muitos clientes para um técnico
+    @JoinColumn(name = "cliente_id")
     private Cliente cliente;
 
     public Chamado() {
--- src/main/java/com/kane/helpdesk/domain/Cliente.java
+++ src/main/java/com/kane/helpdesk/domain/Cliente.java
@@ -3,16 +3,27 @@ package com.kane.helpdesk.domain;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.persistence.Entity;
+import javax.persistence.OneToMany;
+
+import com.kane.helpdesk.domain.enums.Perfil;
+
+@Entity
 public class Cliente extends Pessoa{
+
+    private static final Long serialVersionUID = 1L;
     
+    @OneToMany(mappedBy = "cliente")
     private List<Chamado> chamados = new ArrayList<>();
 
-    public Cliente(List<Chamado> chamados) {
-        this.chamados = chamados;
+    public Cliente() {
+        super();
+        addPerfil(Perfil.CLIENTE);
     }
 
     public Cliente(Integer id, String nome, String cpf, String email, String senha) {
         super(id, nome, cpf, email, senha);
+        addPerfil(Perfil.CLIENTE);
     }
 
     public List<Chamado> getChamados() {
@@ -21,6 +32,31 @@ public class Cliente extends Pessoa{
 
     public void setChamados(List<Chamado> chamados) {
         this.chamados = chamados;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = super.hashCode();
+        result = prime * result + ((chamados == null) ? 0 : chamados.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (!super.equals(obj))
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Cliente other = (Cliente) obj;
+        if (chamados == null) {
+            if (other.chamados != null)
+                return false;
+        } else if (!chamados.equals(other.chamados))
+            return false;
+        return true;
     }
     
 }
