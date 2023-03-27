6af192f74a535885eb7506dae9355e8492c40483
Kane Chan <kanes.chan@gmail.com>
2023-03-27 16:45:07 -0300

    Implementado a liberação de acesso ao H2.

--- src/main/java/com/kane/helpdesk/config/SecurityConfig.java
+++ src/main/java/com/kane/helpdesk/config/SecurityConfig.java
@@ -1,6 +1,8 @@
 package com.kane.helpdesk.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.core.env.Environment;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -13,10 +15,23 @@ import java.util.Arrays;
 
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter{
+
+    private static final String[] PUBLIC_MATCHERS = {"/h2-console/**"};
+
+    @Autowired
+    private Environment env;
     
     @Override
     protected void configure(HttpSecurity http) throws Exception {
+        if(Arrays.asList(env.getActiveProfiles()).contains("test")){
+            http.headers().frameOptions().disable();
+        }
+
         http.cors().and().csrf().disable();
+
+        http.authorizeRequests()
+            .antMatchers(PUBLIC_MATCHERS).permitAll()
+            .anyRequest().authenticated();
 
         http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
     }
