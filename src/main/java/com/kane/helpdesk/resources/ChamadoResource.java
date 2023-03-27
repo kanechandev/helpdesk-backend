e2b6fedcd7dee78499eaf46e9a06f61c47bcd699
Kane Chan <kanes.chan@gmail.com>
2023-03-27 13:36:26 -0300

    Implementado findAll para Chamado.

--- src/main/java/com/kane/helpdesk/resources/ChamadoResource.java
+++ src/main/java/com/kane/helpdesk/resources/ChamadoResource.java
@@ -1,5 +1,8 @@
 package com.kane.helpdesk.resources;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 import org.apache.catalina.connector.Response;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -23,5 +26,12 @@ public class ChamadoResource {
     public ResponseEntity<ChamadoDTO> findById(@PathVariable Integer id){
         Chamado obj = service.findById(id);
         return ResponseEntity.ok().body(new ChamadoDTO(obj));
+    }
+
+    @GetMapping
+    public ResponseEntity<List<ChamadoDTO>> findAll(){
+        List<Chamado> list = service.findAll();
+        List<ChamadoDTO> listDTO = list.stream().map(obj -> new ChamadoDTO(obj)).collect(Collectors.toList());
+        return ResponseEntity.ok().body(listDTO);
     }
 }
