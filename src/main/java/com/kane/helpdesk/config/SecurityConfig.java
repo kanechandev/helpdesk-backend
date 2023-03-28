28295e0fc6e72ed7a289e8aaa9b6836317b3c314
Kane Chan <kanes.chan@gmail.com>
2023-03-28 13:23:34 -0300

    Registrar filtro de autenticação em SecurityConfig e override configure.

--- src/main/java/com/kane/helpdesk/config/SecurityConfig.java
+++ src/main/java/com/kane/helpdesk/config/SecurityConfig.java
@@ -3,14 +3,19 @@ package com.kane.helpdesk.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.core.env.Environment;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import com.kane.helpdesk.security.JWTAuthenticationFilter;
+import com.kane.helpdesk.security.JWTUtil;
 
 import java.util.Arrays;
 
@@ -21,6 +26,10 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
 
     @Autowired
     private Environment env;
+    @Autowired
+    private JWTUtil jwtUtil;
+    @Autowired
+    private UserDetailsService userDetailsService;
     
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -30,11 +39,18 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
 
         http.cors().and().csrf().disable();
 
+        http.addFilter(new JWTAuthenticationFilter(authenticationManager(), jwtUtil));
+
         http.authorizeRequests()
             .antMatchers(PUBLIC_MATCHERS).permitAll()
             .anyRequest().authenticated();
 
         http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userDetailsService).passwordEncoder(bCryptPasswordEncoder());
     }
 
     @Bean
